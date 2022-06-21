63daf5ccb2c8614a6ae56b8dedff33bea51890cf
Devyansh Sehgal <65131029+devyansh3@users.noreply.github.com>
2022-06-22 04:16:21 +0530

    cors"

--- server.js
+++ server.js
@@ -6,16 +6,18 @@ const path = require('path');
 const cors = require('cors');
 // Cors 
 const corsOptions = {
-  origin: process.env.ALLOWED_CLIENTS.split(',')
+  origin: process.env.ALLOWED_CLIENTS.split(','),
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+
   // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
 }
 
 // Default configuration looks like
 // {
 //     "origin": "*",
-//     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-//     "preflightContinue": false,
-//     "optionsSuccessStatus": 204
+    
 //   }
 
 app.use(cors(corsOptions))
